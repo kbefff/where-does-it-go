e432775746cb7d72fe1a291f1a94412efafe1512
kbefff <kbeffel10@me.com>
2019-03-04 07:07:50 -0800

    fabric

---
+++ categories_to_fabric.sql
@@ -0,0 +1,36 @@
+CREATE TABLE fabric (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  categoryId INTEGER(11),
+  fabric_categories VARCHAR(455) NOT NULL,
+  Primary Key (id)
+);
+
+
+
+INSERT INTO fabric (fabric_categories, categoryId) 
+VALUES 
+
+("Bedding or Linens", 4),
+("Carpet or Padding", 4),
+("Clothes or Shoes", 4),
+("Dryer Sheets", 4),
+("Fabric or Textiles", 4),
+("Reusable Shopping Bags", 4)
+
+
+
+SELECT * from categories;
+SELECT * FROM fabric;
+
+SELECT fabric_categories, categoryName
+FROM fabric
+INNER JOIN categories ON fabric.categoryId = categories.id;
+
+
+SELECT fabric_categories, categoryName
+FROM fabric
+LEFT JOIN categories ON fabric.categoryId = categories.id;
+
+SELECT fabric_categories, categoryName
+FROM fabric
+RIGHT JOIN categories ON fabric.categoryId = categories.id;
--- main.sql
+++ main.sql
@@ -148,3 +148,37 @@ FROM electronics
 RIGHT JOIN categories ON electronics.categoryId = categories.id;
 
 -- ============================ FABRIC ============================
+CREATE TABLE fabric (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  categoryId INTEGER(11),
+  fabric_categories VARCHAR(355) NOT NULL,
+  Primary Key (id)
+);
+
+INSERT INTO fabric (fabric_categories, categoryId) 
+VALUES 
+
+("Bedding or Linens", 4),
+("Carpet or Padding", 4),
+("Clothes or Shoes", 4),
+("Dryer Sheets", 4),
+("Fabric or Textiles", 4),
+("Reusable Shopping Bags", 4)
+
+
+
+SELECT * from categories;
+SELECT * FROM fabric;
+
+SELECT fabric_categories, categoryName
+FROM fabric
+INNER JOIN categories ON fabric.categoryId = categories.id;
+
+
+SELECT fabric_categories, categoryName
+FROM fabric
+LEFT JOIN categories ON fabric.categoryId = categories.id;
+
+SELECT fabric_categories, categoryName
+FROM fabric
+RIGHT JOIN categories ON fabric.categoryId = categories.id;

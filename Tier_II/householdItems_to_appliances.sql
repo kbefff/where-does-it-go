020b50a26b94b518a0b3de7c6c14780981c2d5d1
kbefff <kbeffel10@me.com>
2019-03-05 12:42:05 -0800

    tier II work

---
+++ Tier_II/householdItems_to_appliances.sql
@@ -0,0 +1,82 @@
+-- source categories_to_householdItems.sql
+DROP DATABASE IF EXISTS where_does_it_go;
+CREATE DATABASE where_does_it_go;
+USE where_does_it_go;
+
+CREATE TABLE categories (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  categoryName VARCHAR(255) NOT NULL,
+  Primary Key (id)
+);
+
+
+CREATE TABLE householdItems (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  categoryId INTEGER(11),
+  householdItems_categories VARCHAR(255) NOT NULL,
+  Primary Key (id)
+);
+
+INSERT INTO categories (categoryName)
+VALUES ("Household Items"),
+("Construction & Demolition"),
+("Electronics"),
+("Fabric"),
+("Food"),
+("Food Packaging"),
+("Glass & Ceramics"),
+("Hazardous Items"),
+("Metal & Metal Items"),
+("Paper"),
+("Plastic"),
+("Vehicles & Vehicle-Related"),
+("Wood"),
+("Yard Waste");
+
+INSERT INTO householdItems (householdItems_categories, categoryId) 
+VALUES ("Appliances", 1),
+("Batteries", 1),
+("Bathroom & Laundry", 1),
+("Bedroom", 1),
+("Garage & Outdoor", 1),
+("Kitchen", 1),
+("Lights & Bulbs", 1),
+("Misc. Household", 1);
+
+SELECT * from categories;
+SELECT * FROM householdItems;
+
+SELECT householdItems_categories, categoryName
+FROM householdItems
+INNER JOIN categories ON householdItems.categoryId = categories.id;
+
+SELECT householdItems_categories, categoryName
+FROM householdItems
+LEFT JOIN categories ON householdItems.categoryId = categories.id;
+
+SELECT householdItems_categories, categoryName
+FROM householdItems
+RIGHT JOIN categories ON householdItems.categoryId = categories.id;
+
+-- ========================= APPLIANCES =================================
+
+
+CREATE TABLE appliances (
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  householdItems_appliances VARCHAR(255) NOT NULL,
+  Primary Key (id)
+);
+
+INSERT INTO appliances (householdItems_appliances)
+VALUES ('Air Conditioners or Heat Pumps'),
+('Blenders'),
+('Food Processors'),
+('Mixers'),
+('Other Large Appliances'),
+('Refrigerators or Freezers'),
+('Stoves'),
+('Toasters'),
+('Washers or Dryers'),
+('Water Heaters');
+
+SELECT * FROM appliances;

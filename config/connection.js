edcddb241b55af04f242796245f9609b5f9de4e1
kbefff <kbeffel10@me.com>
2019-03-15 10:04:39 -0700

    orm

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 var mysql = require('mysql');
 
 
-// create connection to DB
 var connection = mysql.createConnection({
-    // create connection to my computer
     host: 'localhost',
-    // location in which your server.js file is going to communicate with database
-    port: 3306, // always 3306 in sql 
-    // your computer's user name
+    port: 3306,
     user: 'root',
     password: 'password',
     database: 'where_does_it_go'
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('we are connected as id: ' + connection.threadId);
+});
+
+module.exports = connection;
